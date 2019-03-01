fcc1691b370fa288be0063e3791f14a37549fe27
rmejia-tajc <rmejia.tajc@gmail.com>
2019-03-01 11:30:19 -0700

    add sign up, sign in, see jokes, and sign out functionality to app

--- authentication-app/src/App.js
+++ authentication-app/src/App.js
@@ -1,14 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Route, NavLink, withRouter } from "react-router-dom";
+import axios from "axios";
+
+import "./App.css";
+import Register from "./register/Register.js";
+import Login from "./login/Login.js";
+import Jokes from "./jokes/Jokes.js";
+
+
+// axios defaults and interceptors
+axios.defaults.baseURL = "http://localhost:3300/api";
+axios.interceptors.request.use(
+  function(options) {
+    options.headers.authorization = localStorage.getItem("jwt");
+
+    return options;
+  },
+  function(error) {
+    // do something with the error
+    return Promise.reject(error);
+  }
+);
+
 
 class App extends Component {
+  logout = () => {
+    localStorage.removeItem("jwt");
+    this.props.history.push("/login");
+  };
+
   render() {
     return (
-      <div className="App">
-      <h1>TEST</h1>
-      </div>
+      <>
+        <header>
+          <nav>
+            <NavLink to="/register">Sign Up</NavLink>
+            &nbsp; | &nbsp;
+            <NavLink to="/login">Sign In</NavLink>
+            &nbsp; | &nbsp;
+            <NavLink to="/jokes">Jokes</NavLink>
+            &nbsp; | &nbsp;
+            <button onClick={this.logout}>Sign Out</button>
+          </nav>
+        </header>
+        <section>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/jokes" component={Jokes} />
+        </section>
+      </>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
